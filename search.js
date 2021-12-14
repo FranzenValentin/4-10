d7887f1d0ad21a6f19ff1c16d9544d41ae8e60c3
ValentinFranzen <86569467+ValentinFranzen@users.noreply.github.com>
2021-12-14 23:01:48 +0100

    js auskommentiert

--- search.js
+++ search.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-    $('#search').hover(
+// $(document).ready(function() {
+//     $('#search').hover(
                 
-        function () {
-            $(this).css("animation", "search 1s forwards");
-            $("#text").css("animation", "text 1s forwards");
-            $("#text").css("display", "flex");
-        }, 
+//         function () {
+//             $(this).css("animation", "search 1s forwards");
+//             $("#text").css("animation", "text 1s forwards");
+//             $("#text").css("display", "flex");
+//         }, 
         
-        function () {
-            $(this).css("animation", "search1 1s forwards");
-            $("#text").css("animation", "text1 1s forwards");
-            setTimeout(function (){
-                $("#text").css("display", "none");
-            }, 600);
+//         function () {
+//             $(this).css("animation", "search1 1s forwards");
+//             $("#text").css("animation", "text1 1s forwards");
+//             setTimeout(function (){
+//                 $("#text").css("display", "none");
+//             }, 600);
               
-        }
-    );
+//         }
+//     );
 
-});
+// });
