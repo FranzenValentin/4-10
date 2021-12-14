7fde08329abee57c46da4f407be83270c676aef9
Nils Weyerhäuser <nils.weyerhaeuser@web.de>
2021-12-14 20:52:34 +0100

    .

---
+++ search.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#search').hover(
+                
+        function () {
+            $(this).css("animation", "search 1s forwards");
+            $("#text").css("animation", "text 1s forwards");
+            $("#text").css("display", "flex");
+        }, 
+        
+        function () {
+            /* $(this).removeClass("search-extended");
+            $(this).addClass("search-small"); */
+            
+            $(this).css("animation", "search1 1s forwards");
+            $("#text").css("animation", "text1 1s forwards");
+            setTimeout(function (){
+                $("#text").css("display", "none");
+              }, 600);
+
+        }
+    );
+
+});
