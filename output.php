151db2c7b8e89f1d2cc27877bf0d26c2216c7eda
ValentinFranzen <86569467+ValentinFranzen@users.noreply.github.com>
2021-12-17 18:52:02 +0100

    suche geht

--- output.php
+++ output.php
@@ -19,58 +19,57 @@
         $search = $_GET["search"] ?? null;
     ?>
 
-<form action="output.php" method="get">
-  <input type="search1" name="search" placeholder="🔍Search..." value="<?php echo $search; ?>">
-</form>
+
+    <!-- Such-button -->
+    <form action="output.php" method="get">
+        <input type="search1" name="search" placeholder="🔍Search..." value="<?php echo $search; ?>">
+    </form>
 
     <!-- Tabelle erstellen -->
     <?php
-    
-
-    //   Exit muss direkt nach Search Button kommen (fürs CSS)
-
-    echo '<a href="input.php" id="plus">+</a> ';
+    // Daten abfragen die zur Suche passen, falls eine Suche definiert ist.
+        if (isset($search)) {
+            $t = $search;
+            $search = 'LOWER("%' . $search . '%")'; //LOWER, damit bei der Abfrage die Großschreibung nicht berücksichtigt wird
+            $sql = " SELECT * FROM personen WHERE (LOWER(Nachname) LIKE $search) OR (LOWER(Vorname) LIKE $search) OR (LOWER(Geschlecht) LIKE $search) OR (LOWER(Wohnort) LIKE $search) ";
+            $titel = "Kunden, Suche: $t";
+            echo "<a href='output.php' id='exit'>X</a> ";
+        } 
+        else { //Falls keine Suche ($search) definiert ist, alle Daten der Tabelle abfragen.
         
- 
-    if (isset($search)) {
-        $t = $search;
-        $search = 'LOWER("%' . $search . '%")'; //LOWER, damit bei der ABfrage die Großschreibung nicht berücksichtigt wird
-        $sql = " SELECT * FROM personen WHERE (LOWER(Nachname) LIKE $search) OR (LOWER(Vorname) LIKE $search) OR (LOWER(Geschlecht) LIKE $search) OR (LOWER(Wohnort) LIKE $search) ";
-        $titel = "Kunden, Suche: $t";
-        echo "<a href='output.php' id='exit'>X</a> ";
-    } else {
-        $sql = " SELECT * from personen ";
-        $titel = "Kunden";
-    }
-    $ergebnis = mysqli_query($con, $sql) or die(mysqli_error($con));
-    echo "<h1> $titel</h1>";
+            $sql = " SELECT * from personen ";
+            $titel = "Kunden";
+        }
 
-    echo '    <table id="customers">
-        <tr>
-            <th> Vorname </th>
-            <th> Nachname </th>
-            <th> Geschlecht </th>
-            <th> Wohnort </th>
-            <th id="aktion"> Aktion </th>
+        $ergebnis = mysqli_query($con, $sql) or die(mysqli_error($con));
+        echo "<h1> $titel</h1>";
 
-        </tr>';
-
-    while ($row = mysqli_fetch_array($ergebnis)) {
-        $Vorname = $row['Vorname'];
-        $Nachname = $row['Nachname'];
-        $Geschlecht = $row['Geschlecht'];
-        $Wohnort = $row['Wohnort'];;
-        //Ausgabe mit Links zu bearbeiten und kommentieren
-        echo "
+        echo '    <table id="customers">   
             <tr>
-                <td> $Vorname </td>
-                <td> $Nachname </td>
-                <td> $Geschlecht </td>
-                <td> $Wohnort </td>
-                <td> <a href='bestätigung.php?action=löschen&id_kunden=" . $row['P_ID'] . "'>löschen</a> 
-                <a href='bearbeiten.php?action=bearbeiten&id_kunden=" . $row['P_ID'] . "'>bearbeiten</a> </td></td>
-            </tr> ";
-    }
+                <th> Vorname </th>
+                <th> Nachname </th>
+                <th> Geschlecht </th>
+                <th> Wohnort </th>
+                <th id="aktion"> Aktion </th>
+
+            </tr>';
+
+        while ($row = mysqli_fetch_array($ergebnis)) {
+            $Vorname = $row['Vorname'];
+            $Nachname = $row['Nachname'];
+            $Geschlecht = $row['Geschlecht'];
+            $Wohnort = $row['Wohnort'];;
+            //Ausgabe mit Links zu bearbeiten und kommentieren
+            echo "
+                <tr>
+                    <td> $Vorname </td>
+                    <td> $Nachname </td>
+                    <td> $Geschlecht </td>
+                    <td> $Wohnort </td>
+                    <td> <a href='bestätigung.php?action=löschen&id_kunden=" . $row['P_ID'] . "'>löschen</a> 
+                    <a href='bearbeiten.php?action=bearbeiten&id_kunden=" . $row['P_ID'] . "'>bearbeiten</a> </td></td>
+                </tr> ";
+        }
 
     ?>
     </table>
